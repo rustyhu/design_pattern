8489a1b345e672cce64f5f14648484f5170894c7
rustyhu <rustyhu@hotmail.com>
2018-01-17 12:39:42 +0800

    Debug rust_factory.

--- rust_factory/src/main.rs
+++ rust_factory/src/main.rs
@@ -14,6 +14,7 @@ enum HB_TYPE {
     CHICKEN = 1, 
     FISH = 2, 
     SWEET = 3, 
+    OTHER = 99,
     NOMORE = 0,
 }
 impl From<u32> for HB_TYPE {
@@ -23,10 +24,7 @@ impl From<u32> for HB_TYPE {
             1 => HB_TYPE::CHICKEN,
             2 => HB_TYPE::FISH,
             3 => HB_TYPE::SWEET,
-            _ => {
-                println!("HB_TYPE match exception!");
-                HB_TYPE::NOMORE
-            }
+            _ => HB_TYPE::OTHER,
         }
     }
 }
@@ -43,39 +41,46 @@ fn main() {
         "0. I need no more!\n",
     ];
 
-    let bill_sum = 0;
-    println!("Begin to accept order!");
+    let mut bill_sum = 0;
+    let mut prod_no = 0;
+    fn CheckBill<T: Hamburger>(ham: Box<T>,
+                               ref_no: &mut u32,
+                               ref_bill: &mut u32) {
+        *ref_no += 1;
+        *ref_bill+= ham.getPrice();
+        println!("Product Number: {}, Price: {}.", 
+                 *ref_no,
+                 ham.getPrice());
+    };
 
     let mut asking = String::new();
+    println!("Welcome! Begin to accept order!");
     loop {
         for i in orderMenu.iter() {
             print!("{}", i);
         }
 
-        print!("Please make your choice: ");
+        println!("Please make your choice: ");
+        // stdin().read_line() would execute before print!(),
+        // but not with println!(). Seems a bug.
+        asking = String::new();
         stdin().read_line(&mut asking).expect("Failed to read line");
-        println!();
 
-        let type_ask: u32 = asking.parse().unwrap();
-        if type_ask == (HB_TYPE::NOMORE as u32) {
+        //println!("DEBUGGING: {:?}", asking);
+        let i_ask = asking.trim()
+                          .parse::<u32>()
+                          .expect("Parse to u32 failed!");
+        if i_ask == (HB_TYPE::NOMORE as u32) {
             break;
         }
         //else
         println!("OK! Please wait...");
 
-        let mut prod_no = 0;
-        fn CheckBill<T: Hamburger>(ham: Box<T>, ref_no: &mut u32) {
-            *ref_no += 1;
-            println!("Product Number: {}, Price: {}.", 
-                     *ref_no,
-                     (*ham).getPrice());
-        };
-
-        match HB_TYPE::from(type_ask) {
+        match HB_TYPE::from(i_ask) {
             // start cooking!
-            HB_TYPE::CHICKEN => CheckBill(FactoryBase::<ChickenHb>::Create(), &mut prod_no),
-            HB_TYPE::FISH => CheckBill(FactoryBase::<FishHb>::Create(), &mut prod_no),
-            HB_TYPE::SWEET => CheckBill(FactoryBase::<SweetHb>::Create(), &mut prod_no),
+            HB_TYPE::CHICKEN => CheckBill(FactoryBase::<ChickenHb>::Create(), &mut prod_no, &mut bill_sum),
+            HB_TYPE::FISH => CheckBill(FactoryBase::<FishHb>::Create(), &mut prod_no, &mut bill_sum),
+            HB_TYPE::SWEET => CheckBill(FactoryBase::<SweetHb>::Create(), &mut prod_no, &mut bill_sum),
             _ => println!("Sorry! No this type of hamburger provided now!"),
         }
     }
